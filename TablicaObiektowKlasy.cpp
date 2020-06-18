a8b0479442f3cbadcf26bea13bd9e39076e08c13
Dariusz <vanchesco@icloud.com>
2020-06-18 15:07:33 +0200

    Dodaj pliki projektów.

---
+++ TablicaObiektowKlasy.cpp
@@ -0,0 +1,30 @@
+﻿#include <iostream>
+
+class Channel;
+
+class Buffer
+{
+public:
+	int			dlugosc;
+	int			ilosc;
+	Channel*	B;
+
+	Buffer() : B(new Channel[ilosc]) {};
+};
+
+class Channel
+{
+	double* data;
+
+public:
+	Channel() : data(new double[dlugosc]) {};
+
+};
+
+
+#include <iostream>
+
+int main()
+{
+	Buffer buforek;
+}
